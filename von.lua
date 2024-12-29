62213387a9b212aefc2086f5d4740c6902edb077
V0RTwXh7bA1 <95435758+V0RTwXh7bA1@users.noreply.github.com>
2024-12-28 20:19:01 -0500

    Update von.lua

--- von.lua
+++ von.lua
@@ -8,6 +8,8 @@ local hb = RS.Heartbeat
 local toggle = false
 local character, humanoid
 
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+
 local TeleportCheck = false
 Plr.OnTeleport:Connect(function(State)
 	if (not TeleportCheck) and queueteleport then
